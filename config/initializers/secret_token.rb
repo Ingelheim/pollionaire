73ebd264d578de4847592fc86655c62a19e14a32
Lukas von Ingelheim <lukas.ingelheim@gmail.com>
2013-08-26 20:29:55 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pollionaire::Application.config.secret_key_base = 'd8dcdbffc451e943f0b510a2066c876c5db1a1a64cd4945f9e12a3da8f6f7928aaa83d76b8ad62ff7c2022b334bda60d5513e17924925dd0cc07dccde8a0107e'
