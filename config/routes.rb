909d9f1c50d41cfa482f45c6771e5e2fd42b197f
Lukas von Ingelheim <lukas.ingelheim@gmail.com>
2013-08-28 18:38:11 +0200

    Added static assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pollionaire::Application.routes.draw do
+
+    get '/' => 'landingpage#signup'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +41,7 @@ Pollionaire::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
